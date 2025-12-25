e526675185d81b3bc19b8a8a6c6e2ac9d952fe4d
ardiansetya <ardiansetya129@gmail.com>
2025-12-25 13:43:24 +0700

    create cors

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,24 @@ import { notes } from './routes/notes';
 const app = new Hono()
 
 app.use(logger());
-app.use(cors());
 
 app
-.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw))
-.route('/api/notes', notes)
-.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  .on(
+    ["POST", "GET"],
+    "/api/auth/*",
+    cors({
+      origin: "http://localhost:3001", // replace with your origin
+      allowHeaders: ["Content-Type", "Authorization"],
+      allowMethods: ["POST", "GET", "OPTIONS"],
+      exposeHeaders: ["Content-Length"],
+      maxAge: 600,
+      credentials: true,
+    }),
+    (c) => auth.handler(c.req.raw)
+  )
+  .route("/api/notes", notes)
+  .get("/", (c) => {
+    return c.text("Hello Hono!");
+  });
 
 export default app
